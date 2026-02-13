f745a6babfdf62f2e7faa031e8c23e99cc6fd8f3
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-13 20:46:38 +0545

    Display a single number on expression-ui

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const numberBtns = document.querySelectorAll(".number");
-function getNumber() {
-  numberBtns.forEach((numBtn) => {
-    numBtn.addEventListener("click", (e) => {
-      switch (e.target.innerText) {
-        case "9":
-          return 9;
-        case "8":
-          return 8;
-        case "7":
-          return 7;
-        case "6":
-          return 6;
-        case "5":
-          return 5;
-        case "4":
-          return 4;
-        case "3":
-          return 3;
-        case "2":
-          return 2;
-        case "1":
-          return 1;
-        case "0":
-          return 0;
-      }
-    });
-  });
-}
+const expression = document.querySelector(".expression");
 
+function getNumber(e) {
+  switch (e.target.innerText) {
+    case "9":
+      return 9;
+    case "8":
+      return 8;
+    case "7":
+      return 7;
+    case "6":
+      return 6;
+    case "5":
+      return 5;
+    case "4":
+      return 4;
+    case "3":
+      return 3;
+    case "2":
+      return 2;
+    case "1":
+      return 1;
+    case "0":
+      return 0;
+  }
+}
+numberBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", (e) => {
+    expression.innerText = getNumber(e);
+  });
+});
